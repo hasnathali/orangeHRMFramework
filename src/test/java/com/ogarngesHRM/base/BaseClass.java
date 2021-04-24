e50466e88f2c39181f3926fefce1e83705baaf45
hasnathali <hasnathlu@gmail.com>
2021-04-24 16:28:24 -0400

    added cloudenvironment

--- src/test/java/com/ogarngesHRM/base/BaseClass.java
+++ src/test/java/com/ogarngesHRM/base/BaseClass.java
@@ -1,4 +1,5 @@
 package com.ogarngesHRM.base;
+
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
@@ -16,68 +17,85 @@ import com.ogarngesHRM.dataprovider.ConfigFileReader;
 import com.ogarngesHRM.utility.BrowserFactory;
 
 public class BaseClass {
-	
+
 	public WebDriver driver;// public because need access from testcase
-	
-	
-	public WebDriver getDriver()
-	{
+
+	public WebDriver getDriver() {
 		return driver;
 	}
+
 	/*
-	@Parameters({"Browsr","StagingURL"})
+	 * @Parameters({"Browsr","StagingURL"})
+	 * 
+	 * @BeforeClass public void startBrowser(String Browser, String url) {
+	 * System.out.println("**********Browser is Starting***********");
+	 * BrowserFactory browserfactory = new BrowserFactory(); driver =
+	 * browserfactory.stratBrowser(Browser,url);
+	 * System.out.println("**********Browser is Running***********"); }
+	 */
+	// String osName,String osVersion,String browsrName,String browserVersion,
+	// String applicationURL
+	@Parameters({"runoncloud", "OsName", "OsVersion", "Browsr", "BrowsrVersion", "StagingURL" })
 	@BeforeClass
-	public void startBrowser(String Browser, String url)
+	public void startBrowser(boolean runoncloud, @Optional("Windows") String OsName, @Optional("10") String OsVersion,
+			@Optional("chrome") String Browser, @Optional("88.0") String BrowsrVersion, String url) 
 	{
-		System.out.println("**********Browser is Starting***********");
-		BrowserFactory browserfactory = new BrowserFactory();
-		driver = browserfactory.stratBrowser(Browser,url); 
-		System.out.println("**********Browser is Running***********");
-	}*/
-	//String osName,String osVersion,String browsrName,String browserVersion, String applicationURL
-	@Parameters({"OsName","OsVersion","Browsr","BrowsrVersion","StagingURL"})
-	@BeforeClass
-	public void startBrowser(@Optional("Windows")String OsName,@Optional("10")String OsVersion,@Optional("chrome")String Browser,@Optional("88.0")String BrowsrVersion, String url)
+		if(runoncloud==true)
+		{
+			startBrowseronnGrid(OsName, OsVersion, Browser, BrowsrVersion, url);
+			
+		}
+		else
+		{
+			startBrowseronLocal(Browser, url);
+		}
+	
+	}
+
+	public void startBrowseronnGrid(String OsName, String OsVersion,
+			String Browser, String BrowsrVersion, String url) 
 	{
 		System.out.println("*****Start Browser on BrowserStack with bellow details:*****");
-		
-		System.out.println("OS Name "+ OsName);
-		System.out.println("OS Version "+ OsVersion);
-		System.out.println("Browser Name "+ Browser);
-		System.out.println("Browser Version "+ BrowsrVersion);
-		System.out.println("Application url "+ url);
-		
+
+		System.out.println("OS Name " + OsName);
+		System.out.println("OS Version " + OsVersion);
+		System.out.println("Browser Name " + Browser);
+		System.out.println("Browser Version " + BrowsrVersion);
+		System.out.println("Application url " + url);
+
 		BrowserFactory browserfactory = new BrowserFactory();
 		driver = browserfactory.stratBrowserOnGrid(OsName, OsVersion, Browser, BrowsrVersion, url);
 		System.out.println("**********Browser is to going Start and run the test ***********");
 	}
 
-
-	/*
-	@BeforeClass
-	public void startBrowser()
+	public void startBrowseronLocal(String Browser, String url) 
 	{
 		System.out.println("**********Browser is Starting***********");
 		BrowserFactory browserfactory = new BrowserFactory();
-		//Get Browser name and URL From Configure file 
-		ConfigFileReader config = new ConfigFileReader();
-		String Browsr =config.getProperty("Browser");
-		String URL = config.getProperty("StagingURL");
-		driver = browserfactory.stratBrowser(Browsr,URL); 
-		// OR
-		//driver = browserfactory.stratBrowser(config.getProperty("Browser"), config.getProperty("StagingURL"));
-		//driver = browserfactory.stratBrowser("chrome", "https://opensource-demo.orangehrmlive.com/");
+		driver = browserfactory.stratBrowser(Browser, url);
 		System.out.println("**********Browser is Running***********");
 	}
-	*/
-	
+
+	/*
+	 * @BeforeClass public void startBrowser() {
+	 * System.out.println("**********Browser is Starting***********");
+	 * BrowserFactory browserfactory = new BrowserFactory(); //Get Browser name and
+	 * URL From Configure file ConfigFileReader config = new ConfigFileReader();
+	 * String Browsr =config.getProperty("Browser"); String URL =
+	 * config.getProperty("StagingURL"); driver =
+	 * browserfactory.stratBrowser(Browsr,URL); // OR //driver =
+	 * browserfactory.stratBrowser(config.getProperty("Browser"),
+	 * config.getProperty("StagingURL")); //driver =
+	 * browserfactory.stratBrowser("chrome",
+	 * "https://opensource-demo.orangehrmlive.com/");
+	 * System.out.println("**********Browser is Running***********"); }
+	 */
+
 	@AfterClass
-	public void closeBroswer()
-	{
+	public void closeBroswer() {
 		driver.close();
 		driver.quit();
 		System.out.println("**********Browser is Closing*************");
 	}
-	
 
-}//end class
+}// end class
